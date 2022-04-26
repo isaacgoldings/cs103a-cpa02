b926b2cbf1032f661db7e56b885661a86c7a6b5d
isaacgoldings <99281880+isaacgoldings@users.noreply.github.com>
2022-04-26 12:46:22 -0400

    cleaned up comments and removed unnessecary files

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-  app.js -- This creates an Express webserver with login/register/logout authentication
+  app.js -- This creates an Express 
 */
 
 // *********************************************************** //
@@ -19,7 +19,6 @@ const axios = require("axios")
 //  Loading models
 // *********************************************************** //
 const ToDoItem = require("./models/ToDoItem")
-//const Course = require('./models/Book')
 const Schedule = require('./models/Schedule')
 
 const Book = require('./models/Book')
@@ -28,7 +27,7 @@ const Book = require('./models/Book')
 //  Loading JSON datasets
 // *********************************************************** //
 const books = require('./public/data/books.json')
-//old "courses"
+
 
 
 // *********************************************************** //
@@ -36,9 +35,7 @@ const books = require('./public/data/books.json')
 // *********************************************************** //
 
 const mongoose = require( 'mongoose' );
-//const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
 const mongodb_URI = 'mongodb+srv://Isaac:compass@goldingscpa02-cluster.12muj.mongodb.net/test'
-//mongodb+srv://cs103a:<password>@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 mongoose.connect( mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 // fix deprecation warnings
@@ -93,18 +90,6 @@ app.use(
 // *********************************************************** //
 
 
-// // here is the code which handles all /login /signin /logout routes
-// const auth = require('./routes/auth');
-// const { deflateSync } = require("zlib");
-// app.use(auth)
-
-// // middleware to test is the user is logged in, and if not, send them to the login page
-// const isLoggedIn = (req,res,next) => {
-//   if (res.locals.loggedIn) {
-//     next()
-//   }
-//   else res.redirect('/login')
-// }
 
 // specify that the server should render the views/index.ejs page for the root path
 // and the index.ejs code will be wrapped in the views/layouts.ejs code which provides
@@ -113,146 +98,27 @@ app.get("/", (req, res, next) => {
   res.render("index");
 });
 
-app.get("/about", (req, res, next) => {
-  res.render("about");
-});
 
 
 
-// /*
-//     ToDoList routes
-// */
-// app.get('/todo',
-//   isLoggedIn,   // redirect to /login if user is not logged in
-//   async (req,res,next) => {
-//     try{
-//       let userId = res.locals.user._id;  // get the user's id
-//       let items = await ToDoItem.find({userId:userId}); // lookup the user's todo items
-//       res.locals.items = items;  //make the items available in the view
-//       res.render("toDo");  // render to the toDo page
-//     } catch (e){
-//       next(e);
-//     }
-//   }
-//   )
-
-//   app.post('/todo/add',
-//   isLoggedIn,
-//   async (req,res,next) => {
-//     try{
-//       const {title,description} = req.body; // get title and description from the body
-//       const userId = res.locals.user._id; // get the user's id
-//       const createdAt = new Date(); // get the current date/time
-//       let data = {title, description, userId, createdAt,} // create the data object
-//       let item = new ToDoItem(data) // create the database object (and test the types are correct)
-//       await item.save() // save the todo item in the database
-//       res.redirect('/todo')  // go back to the todo page
-//     } catch (e){
-//       next(e);
-//     }
-//   }
-//   )
-
-//   app.get("/todo/delete/:itemId",
-//     isLoggedIn,
-//     async (req,res,next) => {
-//       try{
-//         const itemId=req.params.itemId; // get the id of the item to delete
-//         await ToDoItem.deleteOne({_id:itemId}) // remove that item from the database
-//         res.redirect('/todo') // go back to the todo page
-//       } catch (e){
-//         next(e);
-//       }
-//     }
-//   )
-
-//   app.get("/todo/completed/:value/:itemId",
-//   isLoggedIn,
-//   async (req,res,next) => {
-//     try{
-//       const itemId=req.params.itemId; // get the id of the item to delete
-//       const completed = req.params.value=='true';
-//       await ToDoItem.findByIdAndUpdate(itemId,{completed}) // remove that item from the database
-//       res.redirect('/todo') // go back to the todo page
-//     } catch (e){
-//       next(e);
-//     }
-//   }
-// )
-
-// /* ************************
-//   Functions needed for the course finder routes
-//    ************************ */
-
-// function getNum(coursenum){
-//   // separate out a coursenum 103A into 
-//   // a num: 103 and a suffix: A
-//   i=0;
-//   while (i<coursenum.length && '0'<=coursenum[i] && coursenum[i]<='9'){
-//     i=i+1;
-//   }
-//   return coursenum.slice(0,i);
-// }
-
-
-// function times2str(times){
-//   // convert a course.times object into a list of strings
-//   // e.g ["Lecture:Mon,Wed 10:00-10:50","Recitation: Thu 5:00-6:30"]
-//   if (!times || times.length==0){
-//     return ["not scheduled"]
-//   } else {
-//     return times.map(x => time2str(x))
-//   }
-  
-// }
-// function min2HourMin(m){
-//   // converts minutes since midnight into a time string, e.g.
-//   // 605 ==> "10:05"  as 10:00 is 60*10=600 minutes after midnight
-//   const hour = Math.floor(m/60);
-//   const min = m%60;
-//   if (min<10){
-//     return `${hour}:0${min}`;
-//   }else{
-//     return `${hour}:${min}`;
-//   }
-// }
-
-// function time2str(time){
-//   // creates a Times string for a lecture or recitation, e.g. 
-//   //     "Recitation: Thu 5:00-6:30"
-//   const start = time.start
-//   const end = time.end
-//   const days = time.days
-//   const meetingType = time['type'] || "Lecture"
-//   const location = time['building'] || ""
-
-//   return `${meetingType}: ${days.join(",")}: ${min2HourMin(start)}-${min2HourMin(end)} ${location}`
-// }
 
 
 
 // /* ************************
 //   Loading (or reloading) the data into a collection
 //    ************************ */
-// // this route loads in the courses into the Course collection
-// // or updates the courses if it is not a new collection
 
 app.get('/upsertDB',
   async (req,res,next) => {
-    //await Course.deleteMany({})
+
     for (b of books){
       const {_id, title, publishedDate}=b;
-      // const num = getNum(coursenum);
-      // b.num=num
-      // b.suffix = coursenum.slice(num.length)
+
       date = "0000-00-00T00:00:00.000-0000"
       if (typeof publishedDate !== "undefined"){
       date = publishedDate.$date;
       b.publishedDate = date;
       }
-      // } else {
-      // const date = Null;
-      // }
 
 
       await Book.findOneAndUpdate({_id, title},b,{upsert:true})
@@ -271,9 +137,8 @@ app.post('/books/byTitle',
     
     res.locals.book = book
     console.log(book)
-    // res.locals.times2str = times2str
-    //res.json(courses)
-    res.render('courselist')
+
+    res.render('bookList')
   }
 )
 
@@ -285,114 +150,22 @@ app.get('/books/show/:bookID',
     res.locals.book = book
 
 
-    res.render('course')
+    res.render('book')
   }
 )
 
 app.post('/books/byCategory',
   // show courses taught by a faculty send from a form
   async (req,res,next) => {
-    console.log(req.body.category);
     const category = req.body.category;
     const books = 
        await Book
                  .find({categories:category})
- //              .sort({title:1})
-   console.log(books)
     res.locals.books = books
     res.render('bookListCat')
   }
 )
 
-// app.get('/courses/show/:courseId',
-//   // show all info about a course given its courseid
-//   async (req,res,next) => {
-//     const {courseId} = req.params;
-//     const course = await Course.findOne({_id:courseId})
-//     res.locals.course = course
-//     res.locals.times2str = times2str
-//     //res.json(course)
-//     res.render('course')
-//   }
-// )
-
-// app.get('/courses/byInst/:email',
-//   // show a list of all courses taught by a given faculty
-//   async (req,res,next) => {
-//     const email = req.params.email+"@brandeis.edu";
-//     const books = await Course.find({instructor:email,independent_study:false})
-//     //res.json(courses)
-//     res.locals.books = books
-//     res.render('courselist')
-//   } 
-// )
-
-// app.post('/courses/byInst',
-//   // show courses taught by a faculty send from a form
-//   async (req,res,next) => {
-//     const email = req.body.email+"@brandeis.edu";
-//     const books = 
-//        await Course
-//                .find({instructor:email,independent_study:false})
-//                .sort({term:1,num:1,section:1})
-//     //res.json(courses)
-//     res.locals.books = books
-//     res.locals.times2str = times2str
-//     res.render('courselist')
-//   }
-// )
-
-// app.use(isLoggedIn)
-
-// app.get('/addCourse/:courseId',
-//   // add a course to the user's schedule
-//   async (req,res,next) => {
-//     try {
-//       const courseId = req.params.courseId
-//       const userId = res.locals.user._id
-//       // check to make sure it's not already loaded
-//       const lookup = await Schedule.find({courseId,userId})
-//       if (lookup.length==0){
-//         const schedule = new Schedule({courseId,userId})
-//         await schedule.save()
-//       }
-//       res.redirect('/schedule/show')
-//     } catch(e){
-//       next(e)
-//     }
-//   })
-
-// app.get('/schedule/show',
-//   // show the current user's schedule
-//   async (req,res,next) => {
-//     try{
-//       const userId = res.locals.user._id;
-//       const courseIds = 
-//          (await Schedule.find({userId}))
-//                         .sort(x => x.term)
-//                         .map(x => x.courseId)
-//       res.locals.books = await Course.find({_id:{$in: courseIds}})
-//       res.render('schedule')
-//     } catch(e){
-//       next(e)
-//     }
-//   }
-// )
-
-// app.get('/schedule/remove/:courseId',
-//   // remove a course from the user's schedule
-//   async (req,res,next) => {
-//     try {
-//       await Schedule.remove(
-//                 {userId:res.locals.user._id,
-//                  courseId:req.params.courseId})
-//       res.redirect('/schedule/show')
-
-//     } catch(e){
-//       next(e)
-//     }
-//   }
-// )
 
 
 // here we catch 404 errors and forward to error handler
